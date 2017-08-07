3b7ec5c702ae589aaa672e5f6076b4b0bf487d18
Demis Bellot <demis.bellot@gmail.com>
2017-08-07 11:22:20 -0400

    refinements

--- src/ProductsPage.cs
+++ src/ProductsPage.cs
@@ -10,7 +10,7 @@ namespace TemplatePages
     public class ProductsPage : TemplateCodePage
     {
         string render(Product[] products) => $@"
-        <table class='table'>
+        <table class='table table-striped'>
             <thead>
                 <tr>
                     <th></th>
