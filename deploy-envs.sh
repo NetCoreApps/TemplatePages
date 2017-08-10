e3d10e58a97b855e8ae178495d3078a998bb9b1a
Demis Bellot <demis.bellot@gmail.com>
2017-08-10 14:34:29 -0400

    change url to use ss.net

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,7 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=templates.netcore.io
+export AWS_VIRTUAL_HOST=templates.servicestack.net
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
